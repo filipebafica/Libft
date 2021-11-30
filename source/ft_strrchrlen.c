1161ed5bd0f3908e598bbc7724bb0f07f9cf01ee
fbafica <fbafica@student.42sp.org.br>
2021-11-30 16:36:37 -0300

    add ft_strrcherlen

--- source/ft_strrchrlen.c
+++ source/ft_strrchrlen.c
@@ -1,27 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strrchrlen.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbafica <fbafica@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/25 16:47:52 by fbafica           #+#    #+#             */
-/*   Updated: 2021/11/27 21:06:16 by fbafica          ###   ########.fr       */
+/*   Created: 2021/11/30 16:19:05 by fbafica           #+#    #+#             */
+/*   Updated: 2021/11/30 16:20:39 by fbafica          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2)
+static	int	get_len(const char *s)
 {
-	size_t	i;
+	int	i;
 
 	i = 0;
-	if (!s1)
-		return ('\0' - s2[i]);
-	if (!s2)
-		return (s1[i] - '\0');
-	while (s1[i] == s2[i] && s1[i] != '\0')
+	while (*s)
+	{
+		++s;
 		++i;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	return (i);
+}
+
+int	ft_strrchrlen(const char *s, int c)
+{
+	int	len;
+	int	i;
+	int	j;
+	int	ii;
+
+	len = get_len(s);
+	i = 0;
+	j = 0;
+	ii = 0;
+	while (i <= len)
+	{
+		if (s[i] == (unsigned char)c)
+		{
+			j = i;
+			ii = 1;
+		}
+		++i;
+	}
+	if (ii == 0)
+		return (ii);
+	else
+		return (j);
 }
